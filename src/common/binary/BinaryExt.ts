e18477e653b7181d4f8d192936a48399ce338051
Alex // timetocode <alex@timetocode.com>
2023-02-21 03:09:52 -0800

    arrays of numbers

--- src/common/binary/BinaryExt.ts
+++ src/common/binary/BinaryExt.ts
@@ -13,11 +13,25 @@ function countString(value: string): number {
     return length + 4
 }
 
-type TypedArray = Uint8Array | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array | Float32Array | Float64Array
+type RegularOrTypedArray = [] | Uint8Array | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array | Float32Array | Float64Array
 
-function countTypedArray(value: TypedArray): number {
-    return value.byteLength + 4
+
+function countByteArray(value: RegularOrTypedArray) {
+    return value.length + 4
 }
+
+function count2ByteArray(value: RegularOrTypedArray) {
+    return (value.length * 2) + 4
+}
+
+function count4ByteArray(value: RegularOrTypedArray) {
+    return (value.length * 4) + 4
+}
+
+function count8ByteArray(value: RegularOrTypedArray) {
+    return (value.length * 8) + 4
+}
+
 const data = {
     [Binary.UInt8]: { bytes: 1, write: 'writeUInt8', read: 'readUInt8' },
     [Binary.Int8]: { bytes: 1, write: 'writeInt8', read: 'readInt8' },
@@ -30,14 +44,14 @@ const data = {
     [Binary.Boolean]: { bytes: 1, write: 'writeUInt8', read: 'readUInt8' },
 
     [Binary.String]: { bytes: -1, write: 'writeString', read: 'readString', count: countString },
-    [Binary.UInt8Array]: { bytes: -1, write: 'writeUInt8Array', read: 'readUInt8Array', count: countTypedArray },
-    [Binary.Int8Array]: { bytes: -1, write: 'writeInt8Array', read: 'readInt8Array', count: countTypedArray },
-    [Binary.UInt16Array]: { bytes: -1, write: 'writeUInt16Array', read: 'readUInt16Array', count: countTypedArray },
-    [Binary.Int16Array]: { bytes: -1, write: 'writeInt16Array', read: 'readInt16Array', count: countTypedArray },
-    [Binary.UInt32Array]: { bytes: -1, write: 'writeUInt32Array', read: 'readUInt32Array', count: countTypedArray },
-    [Binary.Int32Array]: { bytes: -1, write: 'writeInt32Array', read: 'readInt32Array', count: countTypedArray },
-    [Binary.Float32Array]: { bytes: -1, write: 'writeFloat32Array', read: 'readFloat32Array', count: countTypedArray },
-    [Binary.Float64Array]: { bytes: -1, write: 'writeFloat64Array', read: 'readFloat64Array', count: countTypedArray },
+    [Binary.UInt8Array]: { bytes: -1, write: 'writeUInt8Array', read: 'readUInt8Array', count: countByteArray },
+    [Binary.Int8Array]: { bytes: -1, write: 'writeInt8Array', read: 'readInt8Array', count: countByteArray },
+    [Binary.UInt16Array]: { bytes: -1, write: 'writeUInt16Array', read: 'readUInt16Array', count: count2ByteArray },
+    [Binary.Int16Array]: { bytes: -1, write: 'writeInt16Array', read: 'readInt16Array', count: count2ByteArray },
+    [Binary.UInt32Array]: { bytes: -1, write: 'writeUInt32Array', read: 'readUInt32Array', count: count4ByteArray },
+    [Binary.Int32Array]: { bytes: -1, write: 'writeInt32Array', read: 'readInt32Array', count: count4ByteArray },
+    [Binary.Float32Array]: { bytes: -1, write: 'writeFloat32Array', read: 'readFloat32Array', count: count8ByteArray },
+    [Binary.Float64Array]: { bytes: -1, write: 'writeFloat64Array', read: 'readFloat64Array', count: count8ByteArray },
 }
 
 export default function (binaryType: Binary) {
